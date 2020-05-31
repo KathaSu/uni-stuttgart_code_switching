f9ae7cc60e6a27652747a07cfbfc83e8afc60583
Katha <suhr.katharina@gmail.com>
2020-05-31 14:26:49 +0200

    test

--- data_input.py
+++ data_input.py
@@ -1,3 +1,7 @@
+import stanza
+
+nlp = stanza.Pipeline(lang='en', processors='tokenize,ner')
+
 with open("EsEn_NER\calcs_dev.tsv", encoding='utf-8') as file:
     data = file.readlines()
 
@@ -6,9 +10,11 @@ with open("EsEn_NER\calcs_dev.tsv", encoding='utf-8') as file:
     for line in data:
         line  = line.split("\t")        
         if line[0] != t:
+            doc = nlp(tweet) 
+            print(*[f'token: {ent.text}\tner: {ent.type}' for sent in doc.sentences for ent in sent.ents], sep='\n')
             t = line[0]
             tweet = line[4]
         else:
+            
             tweet += " " + line[4] + " "
-    print(tweet)
     
